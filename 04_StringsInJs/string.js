d620b14473787898d43d7efb4ac16c62b58b3063
Mozahid Izhar <165319495+mozahid2004@users.noreply.github.com>
2025-01-13 06:17:14 +0000

    i lean about strings and srings Method in javaScript........

---
+++ 04_StringsInJs/string.js
@@ -0,0 +1,19 @@
+const name = "Mozahid"
+const repoCount = 10
+
+// console.log(name + repoCount + " Values "); out dates Syntax...............
+// use backticks
+// console.log(`My name is ${name} and My Number of rePos ${repoCount}`);
+
+// methods to lowercase
+
+console.log(name[ /*index*/ 0]);
+console.log(name.length);
+console.log(name.charAt(4));
+console.log(name.indexOf('d'));
+console.log(name.toUpperCase());
+
+
+
+
+
