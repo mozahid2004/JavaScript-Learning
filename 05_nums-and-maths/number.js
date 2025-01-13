0348c56fc4891f886f7d0f7233d9ca1a7627a451
Mozahid Izhar <165319495+mozahid2004@users.noreply.github.com>
2025-01-13 06:42:58 +0000

    Number And math with its Meathods

---
+++ 05_nums-and-maths/number.js
@@ -0,0 +1,13 @@
+const score = 300;
+// define the string thought the class
+
+const balance =  new Number (10000);
+
+
+console.log(balance);
+const hundred = 1000000000000;
+
+console.log(hundred.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.round(Math.random()*10));
