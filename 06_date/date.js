d2a39ba4deff1443a8e9f9b3509d626f01cd5226
Mozahid Izhar <165319495+mozahid2004@users.noreply.github.com>
2025-01-14 03:21:25 +0000

    About date details

---
+++ 06_date/date.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+
+
+
